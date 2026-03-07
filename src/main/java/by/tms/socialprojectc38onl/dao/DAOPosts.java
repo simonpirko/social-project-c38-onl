1b8d72a8d43c321054f16ecc5afef35eba27bcd8
Pivovarov98 <legion_19981@mail.ru>
2026-03-07 22:12:58 +0300

    feat: improved adding account data to a post in the findById method

--- src/main/java/by/tms/socialprojectc38onl/dao/DAOPosts.java
+++ src/main/java/by/tms/socialprojectc38onl/dao/DAOPosts.java
@@ -52,19 +52,25 @@ public class DAOPosts {
     public Optional<Post> findById(Integer id) {
         try (Connection connection = PgConnection.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "SELECT a.id AS post_id, a.created_at AS post_created_at, a.title, a.description, a.images, c.id AS comment_id, c.created_at AS comment_created_at, c.text FROM posts a JOIN post_comments c ON a.id = c.post_id WHERE a.id = ?");
+                    "SELECT a.id AS post_id, a.created_at AS post_created_at, a.title, a.description, a.images, acc.id AS account_id, acc.nickname, acc.email, c.id AS comment_id, c.created_at AS comment_created_at, c.text FROM posts a JOIN post_comments c ON a.id = c.post_id  JOIN accounts acc ON acc.id = a.account_id WHERE a.id = ?");
             preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
 
             if (resultSet.next()) {
                 Post post = new Post();
+                Account account = new Account();
                 List<Comment> comments = new ArrayList<>();
+
+                account.setId(resultSet.getInt("account_id"));
+                account.setNickname(resultSet.getString("nickname"));
+                account.setEmail(resultSet.getString("email"));
 
                 post.setId(resultSet.getInt("post_id"));
                 post.setTitle(resultSet.getString("title"));
                 post.setDescription(resultSet.getString("description"));
                 post.setCreateAt(resultSet.getTimestamp("post_created_at"));
                 post.setImages(resultSet.getString("images"));
+                post.setAccount(account);
 
                 while (resultSet.next()) {
                     Comment comment = new Comment();
