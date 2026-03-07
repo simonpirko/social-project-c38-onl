e6c149400379d7b513027f60f8aef5929af3a934
Pivovarov98 <legion_19981@mail.ru>
2026-03-08 01:06:17 +0300

    fix: added fetching posts and passing them to JSP

--- src/main/java/by/tms/socialprojectc38onl/web/servlet/PostPageServlet.java
+++ src/main/java/by/tms/socialprojectc38onl/web/servlet/PostPageServlet.java
@@ -1,7 +1,8 @@
 package by.tms.socialprojectc38onl.web.servlet;
 
-import by.tms.socialprojectc38onl.dao.DAOPosts;
+import by.tms.socialprojectc38onl.models.Comment;
 import by.tms.socialprojectc38onl.models.Post;
+import by.tms.socialprojectc38onl.service.CommentService;
 import by.tms.socialprojectc38onl.service.PostService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -10,6 +11,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -17,6 +19,7 @@ import java.util.Optional;
 public class PostPageServlet extends HttpServlet {
 
     private final PostService postsService = PostService.getInstance();
+    private final CommentService commentService = CommentService.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -28,6 +31,7 @@ public class PostPageServlet extends HttpServlet {
         }
 
         Optional<Post> postOptional = postsService.findById(postId);
+        List<Comment> comments = commentService.findAllComments(postId);
 
         if (postOptional.isEmpty()) {
             resp.setStatus(404);
@@ -35,6 +39,7 @@ public class PostPageServlet extends HttpServlet {
             return;
         }
 
+        req.setAttribute("comments", comments);
         req.setAttribute("post", postOptional.get());
         req.getRequestDispatcher("/pages/postPage.jsp").forward(req, resp);
     }
