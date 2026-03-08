f79bc4ed008d6cbebacf14bd17878df34511789a
Андрей <akovzika@yandex.by>
2026-03-08 23:21:42 +0300

    fix: SQL request for getting post with no comments

--- src/main/java/by/tms/socialprojectc38onl/dao/DAOPosts.java
+++ src/main/java/by/tms/socialprojectc38onl/dao/DAOPosts.java
@@ -52,7 +52,7 @@ public class DAOPosts {
     public Optional<Post> findById(Integer id) {
         try (Connection connection = PgConnection.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "SELECT a.id AS post_id, a.created_at AS post_created_at, a.title, a.description, a.images, c.id AS comment_id, c.created_at AS comment_created_at, c.text FROM posts a JOIN post_comments c ON a.id = c.post_id WHERE a.id = ?");
+                    "SELECT p.id AS post_id, p.created_at AS post_created_at, p.title, p.description, p.images, c.id AS comment_id, c.created_at AS comment_created_at, c.text FROM posts p LEFT JOIN post_comments c ON p.id = c.post_id WHERE p.id = ?");
             preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
 
