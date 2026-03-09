bf976b51ae142e121695d15c1c6cd2a407f7abda
Nikita Rudolf <nikitosr423@gmail.com>
2026-03-09 13:40:48 +0300

    refactor: added SubscribeService, reoptimized account showing logic

--- src/main/java/by/tms/socialprojectc38onl/web/servlet/AccountServlet.java
+++ src/main/java/by/tms/socialprojectc38onl/web/servlet/AccountServlet.java
@@ -3,6 +3,7 @@ package by.tms.socialprojectc38onl.web.servlet;
 import by.tms.socialprojectc38onl.dto.AccountDataDTO;
 import by.tms.socialprojectc38onl.models.Account;
 import by.tms.socialprojectc38onl.service.AccountService;
+import by.tms.socialprojectc38onl.service.SubscribeService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,6 +16,7 @@ import java.util.Objects;
 @WebServlet("/account")
 public class AccountServlet extends HttpServlet {
     private final AccountService accountService = AccountService.getInstance();
+    private final SubscribeService subscribeService = SubscribeService.getInstance();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Account currentAccount = (Account) req.getSession().getAttribute("account");
@@ -23,8 +25,25 @@ public class AccountServlet extends HttpServlet {
             resp.sendRedirect(req.getContextPath() + "/login");
             return;
         }
+        Integer showingAccountId;
+        Integer currentAccountId = currentAccount.getId();
+        String accountId = req.getParameter("id");
 
-        AccountDataDTO accountData = accountService.getAccountData(currentAccount.getId());
+        if (Objects.isNull(accountId) || accountId.isEmpty()) {
+            showingAccountId = currentAccountId;
+            req.setAttribute("isPersonal", true);
+        } else
+        {
+            showingAccountId = Integer.parseInt(accountId);
+
+            if(Objects.equals(showingAccountId, currentAccountId)) {
+                req.setAttribute("isPersonal", true);
+            }
+        }
+        boolean followingStatus = subscribeService.isFollowing(currentAccountId, showingAccountId);
+        req.setAttribute("isFollowing", followingStatus);
+
+        AccountDataDTO accountData = accountService.getAccountData(showingAccountId);
         req.setAttribute("accountData", accountData);
         req.getRequestDispatcher("/pages/account.jsp").forward(req, resp);
     }
